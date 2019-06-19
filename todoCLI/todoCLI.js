26240ccdaa574452a2f86c4ed50ca2da104937c8
Kashito Raiva <tkraiva@gmail.com>
2019-06-18 17:46:16 -0700

    todoCLI.js

--- todoCLI/todoCLI.js
+++ todoCLI/todoCLI.js
@@ -9,6 +9,7 @@ const rl = readline.createInterface({
 });
 
 let list = [];
+let clist=[];
 
 log("Welcome to Todo CLI!");
 
@@ -18,22 +19,31 @@ log("--------------------------------");
 
 
 let loop = function () {
-    rl.question('(v) View \u25C9 (n) New \u25C9 (cX) Complete \u25C9 (dX) Delete \u25C9 (q) Quit \n > ', (answer) => {
+    rl.question('(v) View \u25C9 (n) New \u25C9 (cX) Complete \u25C9 (dX) Delete \u25C9 (q) Quit \n >  ', (answer) => {
 
         if (answer == 'q') {
             log("See you soon!")
             return  rl.close();
         } else if (answer =='n'){
-            rl.question("What needs doing? \n >",(n1)=>{
-
+            rl.question("\n What needs doing? \n > ",(n1)=>{
+                list.push(n1);
+                log("\n ")
+                loop();
             });
-
+          
+           
         } else if (answer.charAt(0) == 'c' ) {
             log("complete works");
         } else if (answer.charAt(0) == 'd' ) {
 
         } else if (answer == 'v') {
-
+            log("\n ")
+            let count = 0;
+            for(let val of list){
+                count++;
+                log(`${count}     ${val}`);
+            }
+            log("\n ")
         }
 
         loop();
